9228dc33da8612bbd5eb55914153d1bf23cb7443
gitboy2 <jon.instjoe@gmail.com>
2018-02-16 17:27:48 -0600

    I added Sysout message "Hello Austin..."

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello Austin 2/16/17");
+        System.out.println("Hello Austin 2/16/17 early");
+        System.out.println("Hello Austin 2/16/18 hella later");
 
     }
 }
+
+
